b5bf6f31d222c011c02400274df278998615e67f
unknown <adrianpredoana23@gmail.com>
2022-05-14 17:28:31 +0300

    New Updates

--- Index.js
+++ Index.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require("body-parser");
+const messagesRouter = require("./router/messagesRouter");
+const utilsRouter = require("./router/utilsRouter")
 
 const app = express();
 app.use(cors());
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//define routes
+app.use("/messages",messagesRouter);
+app.use("/utils",utilsRouter);
+
 const port = process.env.PORT || 8080;
 
-app.get('/', (req,res) => {
-    res.send('Hello World')
-});
-
 app.listen(port, () => {
-console.log(`CloudComputingProiect app listening on port ${port}!`)
+    console.log(`CloudComputingProiect app listening on port ${port}!`)
 });
 
 
